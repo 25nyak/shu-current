b384d8c6f8343399892293cec8c0c241e08b13ac
25nyak <56729621+25nyak@users.noreply.github.com>
2023-11-18 00:21:03 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,298 @@
+// Import the Web3 library at the top level
+import Web3 from 'web3';
+
+// EnhancedStakingPlatform contract ABI
+const contractABI = [[
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "OwnableInvalidOwner",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "OwnableUnauthorizedAccount",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "totalRewards",
+				"type": "uint256"
+			}
+		],
+		"name": "RewardsDistributed",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Staked",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Unstaked",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_account",
+				"type": "address"
+			}
+		],
+		"name": "calculateRewards",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardsRate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "stake",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "stakedBalances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "stakedTimestamps",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "token",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "unstake",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]]; // Paste your provided ABI here
+
+// EnhancedStakingPlatform contract address
+const contractAddress = '0x8c6c44182db04a82237abc14d7c2bbf1904dd67f'; // Replace with your actual contract address
+
+// BSC WebSocket provider URL
+const bscProviderUrl = 'wss://bsc-ws-node.nariox.org'; // Replace with your actual BSC WebSocket provider URL
+
+// Check if Web3 is already defined by some other source (e.g., MetaMask)
+let web3;
+
+if (typeof window.ethereum !== 'undefined') {
+    // Use the MetaMask provider
+    web3 = new Web3(window.ethereum);
+} else {
+    // Fallback to BSC WebSocket provider
+    web3 = new Web3(new Web3.providers.WebsocketProvider(bscProviderUrl));
+}
+
+// Contract instance
+const enhancedStakingPlatformContract = new web3.eth.Contract(contractABI, contractAddress);
+
+// Function to check the wallet and update UI
+async function updateUI() {
+    // Check if MetaMask is installed and connected
+    if (window.ethereum) {
+        try {
+            // Request account access if needed
+            await window.ethereum.enable();
+
+            // Get the account address
+            const accounts = await web3.eth.getAccounts();
+            const accountAddress = accounts[0];
+
+            // Display account address
+            document.getElementById('accountAddress').innerText = accountAddress;
+
+            // Get staked balance
+            const stakedBalance = await enhancedStakingPlatformContract.methods.stakedBalances(accountAddress).call();
+            document.getElementById('stakedBalance').innerText = stakedBalance;
+
+            // Get rewards
+            const rewards = await enhancedStakingPlatformContract.methods.calculateRewards().call();
+            document.getElementById('rewards').innerText = rewards;
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        console.error('No wallet is installed');
+    }
+}
+
+// Call the updateUI function or execute your code when needed
+updateUI();
